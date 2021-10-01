bde559b8711d6d1202a16de59501fb216d4dd641
Allison Homem <mrs.allisonhomem@outlook.com>
2021-10-01 12:51:34 -0500

    created validateActionId middleware, and wrote get action by id method to verify it's working

--- api/actions/actions-middlware.js
+++ api/actions/actions-middlware.js
@@ -1,1 +1,25 @@
-// add middlewares here related to actions
+//imports
+const Actions = require('./actions-model.js');
+
+//MIDDLEWARE FUNCTIONS
+//validates that an action exists with given id
+async function validateActionId(req,res,next){
+    try{
+        const {id} = req.params
+        const action = await Actions.get(id)
+
+        if(!action){
+            res.status(404).json({message: "no action exists with that id"})
+        }
+        else{
+            req.action = action;
+            next();
+        }
+    }
+    catch{
+        res.status(500).json({message: "an error occurred fetching the action with that id"})
+    }
+}
+
+//exports
+module.exports = {validateActionId};
--- api/actions/actions-router.js
+++ api/actions/actions-router.js
@@ -1,6 +1,27 @@
 //imports
 const express = require('express');
 const router = express.Router();
+const {validateActionId} = require('./actions-middlware.js');
+const Actions = require('./actions-model.js');
+
+//ENDPOINTS
+//Returns an array of actions (or an empty array) as the body of the response.
+
+//Returns an action with the given `id` as the body of the response.
+router.get('/:id', validateActionId, (req, res) => {
+    const {id} = req.params
+
+    Actions.get(id)
+           .then(action => {
+               res.status(200).json(action)
+           })
+})
+
+//Returns the newly created action as the body of the response.
+
+//Returns the updated action as the body of the response.
+
+//Returns no response body.
 
 //exports
 module.exports = router;
