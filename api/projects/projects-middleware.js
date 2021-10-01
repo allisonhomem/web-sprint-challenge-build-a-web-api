92e15cb5996f5f07ba7156cff31edb832595a724
Allison Homem <mrs.allisonhomem@outlook.com>
2021-10-01 11:52:07 -0500

    added middleware function validateProjectNameDes and added it to appropriate methods

--- api/projects/projects-middleware.js
+++ api/projects/projects-middleware.js
@@ -22,5 +22,31 @@ async function validateProjectId(req, res, next){
     }
 }
 
+//checks if request body has name and description
+async function validateProjectNameDes(req,res,next){
+    try {
+        const {name, description} = req.body
+
+        if(!name || !description){
+            res.status(400).json({message: "please fill out all fields"})
+        }
+        else{
+            next();
+        }
+    }
+    catch{
+        res.status(500).json({message: "an error occurred while validating project body"})
+    }
+}
+
+//checks that completed field is not empty
+// async function validateProjectCompleted(req,res,next){
+//     try{
+//         const {completed} = req.body
+
+//         if(completed.notHere)
+//     }
+// }
+
 //exports
-module.exports = {validateProjectId};
+module.exports = {validateProjectId, validateProjectNameDes};
--- api/projects/projects-router.js
+++ api/projects/projects-router.js
@@ -1,13 +1,17 @@
 //imports
 const express = require('express');
 const router = express.Router();
-const {validateProjectId} = require('./projects-middleware.js');
+const {validateProjectId, validateProjectNameDes} = require('./projects-middleware.js');
+const Projects = require('./projects-model.js');
 
 
 //ENDPOINTS
 //Returns an array of projects as the body of the response.
-router.get('/', async (req, res) => {
-
+router.get('/', (req, res) => {
+    Projects.get()
+            .then(projects => {
+                res.status(200).json(projects)
+            })
 })
 
 //Returns a project with the given `id` as the body of the response.
@@ -16,12 +20,12 @@ router.get('/:id', validateProjectId, async (req,res) => {
 })
 
 //Returns the newly created project as the body of the response.
-router.post('/', async (req,res) => {
+router.post('/', validateProjectNameDes, async (req,res) => {
 
 })
 
 //Returns the updated project as the body of the response.
-router.put('/:id', validateProjectId, async (req,res) => {
+router.put('/:id', validateProjectNameDes,validateProjectId, async (req,res) => {
 
 })
 
@@ -32,7 +36,7 @@ router.delete('/:id', validateProjectId, async (req,res) => {
 
 //Returns an array of actions (could be empty) belonging to a project with the given `id`.
 router.get('/:id/actions', validateProjectId, async (req,res) => {
-    
+
 })
 
 //exports
