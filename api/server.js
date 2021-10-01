ca4591ff914812eb7c86e3ee825494823e5b6167
Allison Homem <mrs.allisonhomem@outlook.com>
2021-10-01 10:43:53 -0500

    built out server.js

--- api/server.js
+++ api/server.js
@@ -1,9 +1,15 @@
+//imports
 const express = require('express');
 const server = express();
 
-// Configure your server here
-// Build your actions router in /api/actions/actions-router.js
-// Build your projects router in /api/projects/projects-router.js
-// Do NOT `server.listen()` inside this file!
+//building routers
+const projectsRouter = require('./projects/projects-router.js');
+const actionsRouter = require('./actions/actions-router.js');
 
+//configuring server
+server.use(express.json());
+server.use('/api/projects', projectsRouter);
+server.use('/api/actions', actionsRouter);
+
+//exports
 module.exports = server;
